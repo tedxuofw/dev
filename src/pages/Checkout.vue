40da888585f28e07c1124631b0c6bf993662ca2d
Andrey Butenko <ghostmancer@gmail.com>
2019-01-16 15:02:20 -0800

    Add ticketing functionality

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -9,16 +9,37 @@
         <h1>Checkout</h1>
       </div>
     </div>
-    <div class ="row">
+    <div class="row" v-for="(ticket, ticketIndex) in tickets" :key="ticketIndex">
       <div class="col-6">
         <Ticket
           conferenceTitle="Two Steps Forward" conferenceYear="2019"
-          personName="Andrey" personMeal="Vegan"
-          ticketType="UW Student Ticket" confirmationCode="SB1103"
+          :personName="ticket.name" :personMeal="ticket.meal"
+          :ticketType="ticket.ticket" :confirmationCode="`SB110${ticketIndex}`"
           maxWidth="300px" />
       </div>
       <div class="col-6">
+        <h2>Name</h2>
+        <input type="text" placeholder="First Name" class="full-width" v-model="ticket.name" >
 
+        <h2>Meal Type</h2>
+        <select v-model="ticket.ticket">
+          <option disabled value="">Select a ticket</option>
+          <option>General Ticket</option>
+          <option>UW Student Ticket</option>
+        </select>
+
+        <h2>Meal Type</h2>
+        <select v-model="ticket.meal">
+          <option disabled value="">Select a meal</option>
+          <option>None</option>
+          <option>Vegan</option>
+          <option>Non-Vegan</option>
+        </select>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <button @click="addTicket()">Add another ticket</button>
       </div>
     </div>
   </div>
@@ -31,9 +52,23 @@ export default {
   components: { Ticket },
   data() {
     return {
+      tickets: []
     };
   },
+  mounted() {
+    this.addTicket();
+  },
   methods: {
+    addTicket() {
+      this.tickets.push(this.getEmptyTicket())
+    },
+    getEmptyTicket() {
+      return {
+        name: '',
+        meal: '',
+        ticket: ''
+      };
+    }
   }
 };
 </script>
