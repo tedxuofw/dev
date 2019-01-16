8defc5be5205c5f58dc20c63090775e5f79cc5eb
Andrey Butenko <ghostmancer@gmail.com>
2019-01-16 13:43:41 -0800

    Create checkout route

---
+++ src/pages/Checkout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container components-page">
+    <div class="row">
+      <div class="col-12">
+        <div>
+          <router-link to="/">Components</router-link> &middot;
+          <router-link to="/checkout">Checkout</router-link>
+        </div>
+        <h1>Checkout</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckoutPage",
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/_variables.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ComponentsPage from '@/pages/Components'
+import CheckoutPage from '@/pages/Checkout'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'ComponentsPage',
       component: ComponentsPage
+    },
+    {
+      path: '/checkout',
+      name: 'CheckoutPage',
+      component: CheckoutPage
     }
   ]
 })
