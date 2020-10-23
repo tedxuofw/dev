65906208f488bbc582f66cfe356285f523424d52
gauravgowda <ggowda@cs.washington.edu>
2020-10-22 21:37:18 -0700

    Updated Home Page to allow for speaker nominations

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,8 +12,8 @@
     </div>
       <div class="section-landing-child">
         <h1><a class="highlighted-red" href=''>2021</a></h1>
-        <p style="color: #999997; font-size: 1.1rem; padding-top: 0.5rem"> TEDxUofW Team Applications Now Open </p>
-        <a class="home-page-button" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSd2TFEHDXXC1spfz3rUbpO2WzZUNMg6BpsKNjjZR6jWmzHw0g/viewform">APPLY</a>
+        <p style="color: #999997; font-size: 1.1rem; padding-top: 0.5rem"> TEDxUofW 2021 Speaker Nomination Form Now Open </p>
+        <a class="home-page-button" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScFL9fvlKXcNOck3b7rW2JcsMAHdYHqSvrL6B7CzjHO17sKJw/viewform?usp=sf_link">NOMINATE</a>
       </div>
     </div>
 
